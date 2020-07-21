82acbd58d5e784f9b9042b41d4d414db9e80b868
dhkim92 <92hu@kakao.com>
2020-07-21 23:03:34 +0900

    #Update chrome-app-clone-2020

---
+++ index.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+/* title.innerHTML = "Fuck youuuuuuuuu";
+title.style.color = "red";
+console.log(title);
+ */
+
+function handleResize() {
+  console.log("I have been resized");
+}
+
+window.addEventListener("resize", handleResize);
