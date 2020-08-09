cf7bb7bbb93e2f6973e7b64ce8c02036cd152e45
dhkim92 <92hu@kakao.com>
2020-08-09 17:05:33 +0900

    Update JS Calendar -wip ( #2 )
    Update JS Calendar -wip ( #2 )

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,8 +1,11 @@
 const currentTitle = document.querySelector("#current-year-month");
 const calendarBody = document.querySelector("#calendar-body");
-const today = new Date();
-const first = new Date(today.getFullYear(), today.getMonth(), 1);
-var dayList = [
+const mainTodayDay = document.querySelector("#prev-d-day");
+const mainTodayDate = document.querySelector("#prev-date");
+let today = new Date();
+let first = new Date(today.getFullYear(), today.getMonth(), 1);
+
+const dayList = [
   "Sunday",
   "Monday",
   "Tuesday",
@@ -11,7 +14,7 @@ var dayList = [
   "Friday",
   "Saturday",
 ];
-var monthList = [
+const monthList = [
   "Jan",
   "Feb",
   "Mar",
@@ -25,37 +28,143 @@ var monthList = [
   "Nov",
   "Dec",
 ];
+/* 윤년일경우 */
 const leapYear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+/* 윤년이 아닐경우 */
 const notLeapYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 var pageFirst = first;
 var pageYear;
 
+/* 윤년계산 leapYear = 윤년 (현제 년도를 4로 나눴을때 나머지가 0이면 그 해는 윤년이다.) */
 if (first.getFullYear() % 4 === 0) {
   pageYear = leapYear;
 } else {
   pageYear = notLeapYear;
 }
-
+/*  */
 function showCalendar() {
+  currentTitle.innerHTML =
+    monthList[first.getMonth()] +
+    "&nbsp;&nbsp;&nbsp;&nbsp;" +
+    first.getFullYear();
   let monthCnt = 100;
   let cnt = 1;
-  for (var i = 0; i < 6; i++) {
-    var $tr = document.createElement("tr");
-    $tr.setAttribute("id", monthCnt);
+  for (let i = 0; i < 6; i++) {
+    var createTr = document.createElement("tr");
+    calendarBody.appendChild(createTr);
+    createTr.setAttribute("id", monthCnt);
     for (var j = 0; j < 7; j++) {
-      if ((i === 0 && j < first.getDay()) || cnt > pageYear[first.getMonth()]) {
-        var $td = document.createElement("td");
-        $tr.appendChild($td);
+      if (
+        (i === 0 && j < first.getDay()) /* 토요일 = 6 */ ||
+        cnt > pageYear[first.getMonth() /* 8월=7 */]
+      ) {
+        var createTd = document.createElement("td");
+        createTr.appendChild(createTd);
       } else {
-        var $td = document.createElement("td");
-        $td.textContent = cnt;
-        $td.setAttribute("id", cnt);
-        $tr.appendChild($td);
+        var createTd = document.createElement("td");
+        createTr.appendChild(createTd);
+        createTd.setAttribute("id", cnt);
+        createTd.innerHTML = cnt;
         cnt++;
       }
     }
     monthCnt++;
-    calendarBody.appendChild($tr);
   }
 }
 showCalendar();
+
+function showMain() {
+  mainTodayDay.innerHTML = dayList[today.getDay()];
+  mainTodayDate.innerHTML = today.getDate();
+}
+showMain();
+
+function removeCalendar() {
+  let catchTr = 100;
+  for (var i = 100; i < 106; i++) {
+    var createTr = document.getElementById(catchTr);
+    createTr.remove();
+    catchTr++;
+  }
+}
+
+function prev() {
+  if (pageFirst.getMonth() === 1) {
+    pageFirst = new Date(first.getFullYear() - 1, 12, 1);
+    first = pageFirst;
+    if (first.getFullYear() % 4 === 0) {
+      pageYear = leapYear;
+    } else {
+      pageYear = notLeapYear;
+    }
+  } else {
+    pageFirst = new Date(first.getFullYear(), first.getMonth() - 1, 1);
+    first = pageFirst;
+  }
+  today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+  currentTitle.innerHTML =
+    monthList[first.getMonth()] +
+    "&nbsp;&nbsp;&nbsp;&nbsp;" +
+    first.getFullYear();
+  removeCalendar();
+  showCalendar();
+  showMain();
+  clickedDate1 = document.getElementById(today.getDate());
+  clickedDate1.classList.add("active");
+  clickStart();
+}
+
+function next() {
+  if (pageFirst.getMonth() === 12) {
+    pageFirst = new Date(first.getFullYear() + 1, 1, 1);
+    first = pageFirst;
+    if (first.getFullYear() % 4 === 0) {
+      pageYear = leapYear;
+    } else {
+      pageYear = notLeapYear;
+    }
+  } else {
+    pageFirst = new Date(first.getFullYear(), first.getMonth() + 1, 1);
+    first = pageFirst;
+  }
+  today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
+  currentTitle.innerHTML =
+    monthList[first.getMonth()] +
+    "&nbsp;&nbsp;&nbsp;&nbsp;" +
+    first.getFullYear();
+  removeCalendar();
+  showCalendar();
+  showMain();
+  clickedDate1 = document.getElementById(today.getDate());
+  clickedDate1.classList.add("active");
+  clickStart();
+}
+
+/*  */
+
+var clickedDate1 = document.getElementById(today.getDate());
+clickedDate1.classList.add("active");
+var prevBtn = document.getElementById("prev");
+var nextBtn = document.getElementById("next");
+prevBtn.addEventListener("click", prev);
+nextBtn.addEventListener("click", next);
+var tdGroup = [];
+function clickStart() {
+  for (let i = 1; i <= pageYear[first.getMonth()]; i++) {
+    tdGroup[i] = document.getElementById(i);
+    tdGroup[i].addEventListener("click", changeToday);
+  }
+}
+function changeToday(e) {
+  for (let i = 1; i <= pageYear[first.getMonth()]; i++) {
+    if (tdGroup[i].classList.contains("active")) {
+      tdGroup[i].classList.remove("active");
+    }
+  }
+  clickedDate1 = e.currentTarget;
+  clickedDate1.classList.add("active");
+  today = new Date(today.getFullYear(), today.getMonth(), clickedDate1.id);
+  showMain();
+  keyValue = today.getFullYear() + "" + today.getMonth() + "" + today.getDate();
+}
+clickStart();
---
+++ src/js/prevnext.js
@@ -0,0 +1,60 @@
+function prev() {
+  if (pageFirst.getMonth() === 1) {
+    pageFirst = new Date(first.getFullYear() - 1, 12, 1);
+    first = pageFirst;
+    if (first.getFullYear() % 4 === 0) {
+      pageYear = leapYear;
+    } else {
+      pageYear = notLeapYear;
+    }
+  } else {
+    pageFirst = new Date(first.getFullYear(), first.getMonth() - 1, 1);
+    first = pageFirst;
+  }
+  today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+  currentTitle.innerHTML =
+    monthList[first.getMonth()] +
+    "&nbsp;&nbsp;&nbsp;&nbsp;" +
+    first.getFullYear();
+  /*     removeCalendar();
+  showCalendar();
+  showMain(); */
+  clickedDate1 = document.getElementById(today.getDate());
+  clickedDate1.classList.add("active");
+  /*   clickStart(); */
+  /*   reshowingList(); */
+}
+
+function next() {
+  if (pageFirst.getMonth() === 12) {
+    pageFirst = new Date(first.getFullYear() + 1, 1, 1);
+    first = pageFirst;
+    if (first.getFullYear() % 4 === 0) {
+      pageYear = leapYear;
+    } else {
+      pageYear = notLeapYear;
+    }
+  } else {
+    pageFirst = new Date(first.getFullYear(), first.getMonth() + 1, 1);
+    first = pageFirst;
+  }
+  today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
+  currentTitle.innerHTML =
+    monthList[first.getMonth()] +
+    "&nbsp;&nbsp;&nbsp;&nbsp;" +
+    first.getFullYear();
+  /*     removeCalendar();
+  showCalendar();
+  showMain(); */
+  clickedDate1 = document.getElementById(today.getDate());
+  clickedDate1.classList.add("active");
+  /*   clickStart(); */
+  /*   reshowingList(); */
+}
+today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
+currentTitle.innerHTML =
+  monthList[first.getMonth()] +
+  "&nbsp;&nbsp;&nbsp;&nbsp;" +
+  first.getFullYear();
+next();
+prev();
