2223099dd748f9c693e93b72415f1392263ee588
dhkim92 <92hu@kakao.com>
2020-08-16 06:47:35 +0900

    Fix digclock

--- reclock.js
+++ reclock.js
@@ -2,15 +2,15 @@ const digClockContainer = document.querySelector(".clock-wrap");
 const digClock = document.querySelector("#digClock");
 
 function digitalClock() {
-    function getTime() {
-        const date = new Date();
-        const minutes = date.getMinutes();
-        const hours = date.getHours();
-        digClock.innerText = `${hours}:${
-      minutes < 10 ? `0${minutes}` : `${minutes}`
+  function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    digClock.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+      minutes < 10 ? `0${minutes}` : minutes
     }`;
-    }
-    getTime();
-    setInterval(getTime, 1000);
+  }
+  getTime();
+  setInterval(getTime, 1000);
 }
 digitalClock();
